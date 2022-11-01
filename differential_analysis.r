2904fe5f902ccd2754d9fc852608473630b44266
Gayu-25 <114131834+Gayu-25@users.noreply.github.com>
2022-11-01 16:16:19 +0530

    Update differential_analysis.r

--- differential_analysis.r
+++ differential_analysis.r
@@ -1,15 +1,68 @@
-data<- read.csv("D:/MSc Bioinformatics/Sem3/Cancer genomics/raw_counts.csv")
-View(data)
+setwd("C:\\Users\\gayat\\OneDrive\\Documents\\R")
+library(RCircos)
+data("UCSC.HG38.Human.CytoBandIdeogram")
+cyto.info = UCSC.HG38.Human.CytoBandIdeogram
+RCircos.Set.Core.Components(cyto.info, 
+                            chr.exclude=NULL, 
+                            tracks.inside=10, 
+                            tracks.outside=0)
 
-#Create a count per matrix
-cpmatrix=data
-for(i in 1:ncol(data)){
-  cpmatrix[,i]=(data[,i]/sum(data[,i]))*1000000
-}
-#cpmatrix[is.na(cpmatrix)]=0
+RCircos.Set.Plot.Area()
+RCircos.Chromosome.Ideogram.Plot()
 
-#Calculate a log of cpm
-logcpm=log2(cpmatrix+1)
-print(logcpm)
+cyto.info = UCSC.HG38.Human.CytoBandIdeogram
+cyto.info$Name = NA
+cyto.info$Stain = NA
+RCircos.Set.Core.Components(cyto.info, 
+                            chr.exclude=NULL, 
+                            tracks.inside=10, 
+                            tracks.outside=0)
 
-summary(logcpm)
+chr_order = unique(cyto.info$Chromosome)
+
+RCircos.Set.Plot.Area()
+RCircos.Chromosome.Ideogram.Plot()
+
+ideo = RCircos.Get.Plot.Ideogram()
+ideo$BandColor = 'salmon'
+num = which(ideo$Chromosome == 'chrX')
+ideo[num, 'BandColor'] = 'orchid3'
+
+num = which(ideo$Chromosome == 'chrY')
+ideo[num, 'BandColor'] = 'violetred3'
+
+
+RCircos.Reset.Plot.Ideogram(ideo)
+RCircos.Set.Plot.Area()
+
+
+RCircos.Chromosome.Ideogram.Plot()
+
+library(biomaRt)
+saveRDS(mat,file= "mat_DEG.rds")
+mat = readRDS('mat_DEG.rds')
+
+m = useMart('ensembl', dataset='hsapiens_gene_ensembl')
+
+coords = getBM(attributes=c('chromosome_name', 'start_position', 
+                            'end_position', 'hgnc_symbol'),
+               filters = c('hgnc_symbol'),
+               values = list(rownames(mat)),
+               mart = m)
+
+write.csv(coords, file = 'coords.csv')
+coords$chromosome_name = paste0('chr', coords$chromosome_name)
+coords$chromosome_name = factor(coords$chromosome_name, levels = chr_order)
+
+num = which(is.na(coords$chromosome_name))
+coords = coords[-num, ]
+
+up = which((mat$pval < 0.01) &
+             (mat$log2FC > 1))
+upmat = mat[up, ]
+
+num = which(coords$hgnc_symbol %in% rownames(upmat))
+coords1 = coords[num, ]
+
+RCircos.Gene.Name.Plot(coords1, name.col=4, track.num = 2, side = "in",
+                       is.sorted = F)
